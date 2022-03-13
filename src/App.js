6cc159b86aaea6da912912d87bc241e1fd552b88
Albin <albinjiji4@gmail.com>
2022-03-13 10:09:45 +0530

    Poster component created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Banner from "./Components/Banner/Banner";
 import Navbar from "./Components/Navbar/Navbar";
+import Rowpost from "./Components/RowPost/Rowpost";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Banner />
+      <Rowpost />
     </div>
   );
 }
