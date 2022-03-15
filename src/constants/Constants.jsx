37abb64baeedc87467d028305d20387e4aa56a6a
Albin <albinjiji4@gmail.com>
2022-03-15 13:32:31 +0530

    Update Constants.jsx

--- src/constants/Constants.jsx
+++ src/constants/Constants.jsx
@@ -3,6 +3,3 @@ export const baseUrl = "https://api.themoviedb.org/3";
 export const API_KEY = "099513b85069606d79af91395105b21e";
 
 export const imageUrl = "https://image.tmdb.org/t/p/original";
-
-export const SEARCH_API =
-  "https://api.themoviedb.org/3/search/movie?api_key=&query=";
